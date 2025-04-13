9951ae214738c1671f942291b1b653f48ad80ddb
vanyauhalin <hi@vanyauhalin.me>
2025-04-13 14:39:28 +0400

    bump @vanyauhalin/eslint-config to 0.0.6

--- eslint.config.js
+++ eslint.config.js
@@ -9,12 +9,4 @@ export default [
 			"stylistic/max-statements-per-line": "off",
 		},
 	},
-	{
-		files: ["fixtures/*.js", "lib/*.js"],
-		// todo: these rules must be a part of the eslint-config package
-		rules: {
-			"stylistic/quotes": ["error", "double", {avoidEscape: true, allowTemplateLiterals: true}],
-			"typescript/ban-ts-comment": "off",
-		},
-	},
 ]
