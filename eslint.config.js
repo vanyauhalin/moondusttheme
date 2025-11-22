cad6cacd3da3fde94b7bcfcf71a0bb609f8a55df
vanyauhalin <hi@vanyauhalin.me>
2025-11-22 18:55:43 +0400

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,32 @@ import config from "@vanyauhalin/eslint-config"
 export default [
 	...config,
 	{
-		files: ["fixtures/*.js"],
+		files: ["langs/*/test.js"],
 		rules: {
 			"github/unescaped-html-literal": "off",
+			"stylistic/max-len": "off",
 			"stylistic/max-statements-per-line": "off",
+		},
+	},
+	{
+		files: ["cli.js"],
+		rules: {
+			"github/unescaped-html-literal": "off",
+			"typescript/no-dynamic-delete": "off",
+			"unicorn/import-style": "off",
+		},
+	},
+	{
+		files: ["lang-sample.d.ts"],
+		rules: {
+			"no-var": "off",
+			"prefer-let/prefer-let": "off",
+		},
+	},
+	{
+		files: ["package.json"],
+		rules: {
+			"jsonc/array-bracket-newline": "off",
 		},
 	},
 ]
